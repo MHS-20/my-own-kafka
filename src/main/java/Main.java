798f39128531191bfa9dfba44eaf3fef1e2ee7c0
Sentinel <muhamad.salman2001@gmail.com>
2025-01-17 15:44:03 +0100

    stage 3 - 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,16 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.err.println("Logs from your program will appear here!");
 
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     int port = 9092;
-    
+
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
@@ -19,20 +20,26 @@ public class Main {
       clientSocket = serverSocket.accept();
 
       // get fields from request
-      byte[] message_size = new byte[4]; 
-      byte[] request_api_key = new byte[2]; 
+      byte[] message_size = new byte[4];
+      byte[] request_api_key = new byte[2];
       byte[] request_api_version = new byte[2];
-      byte[] correlation_id = new byte[4];       
+      byte[] correlation_id = new byte[4];
 
       clientSocket.getInputStream().read(message_size);
       clientSocket.getInputStream().read(request_api_key);
       clientSocket.getInputStream().read(request_api_version);
       clientSocket.getInputStream().read(correlation_id);
 
+      // check api version
+      byte[] error_code; 
+      int apiV = ByteBuffer.wrap(request_api_version).getInt();
+      if (apiV > 4)
+        error_code = new byte[] { 0, 23 };
+      else
+        error_code = new byte[] { 0, 0 };
+
       // send response
-      message_size = new byte[]{0, 0, 0, 4};
-      byte[] error_code = new byte[]{0, 0, 0, 23}; //35 = 23hex
-        
+      message_size = new byte[] { 0, 0, 0, 4 };
       clientSocket.getOutputStream().write(message_size);
       clientSocket.getOutputStream().write(correlation_id);
       clientSocket.getOutputStream().write(error_code);
